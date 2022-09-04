d9000ce4d08e1f3d1a9f4dd26abe5ba51b8fd83f
Maksym-Semenov <mestcomua@gmail.com>
2022-09-04 17:52:35 +0300

    Добавьте файлы проекта.

---
+++ Credit Card Mask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Credit_Card_Mask
+{
+    public static class Kata
+    {
+        // return masked string
+        public static string Maskify(string cc)
+        {
+            string pattern = "\w\s";
+            string replacement = "#";
+            Regex rgx = new Regex(pattern);
+            string result = rgx.Replace(cc, replacement);
+
+            return result;
+            //string[] chars = cc.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //if (chars.Length < 4)
+            //{
+            //    return cc;
+            //}
+           
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string chars1 = Kata.Maskify("4556364607935616");
+                Console.WriteLine(chars1);
+            string chars2 = Kata.Maskify("1");
+            Console.WriteLine(chars2);
+            string chars3 = Kata.Maskify("11111");
+                Console.WriteLine(chars3);
+        }
+    }
+}
